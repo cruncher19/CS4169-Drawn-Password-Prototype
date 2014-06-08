c25bea76b74ec8dc0c5db0bc47bd0b403b988434
Brad Covey <bradcovey@gmail.com>
2014-06-07 21:35:53 -0300

    Finished integrating the login process :D

--- website/login.php
+++ website/login.php
@@ -1,26 +1,33 @@
 <!DOCTYPE html public "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/tr/HTML401/loose.dtd">
 <?php
+$file = fopen("test.log","a");
+fwrite($file, "Attempt logged");
+fclose($file);
 
 $l = 0; #inital state of login success
-$uname = trim($_GET["u"]);
-$passId = trim($_GET["p"]);
-$pwd = trim($_GET["pwd"]);
-$attemptDuration = trim($_GET["attemptDuration"]);
-$attemptCount = trim($_GET['attemptCount']);
+$uname = trim($_POST["u"]);
+$passId = trim($_POST["p"]);
+$pwd = trim($_POST["pwd"]);
+$attemptDuration = trim($_POST["attemptDuration"]);
+$attemptCount = trim($_POST['attemptCount']);
 $r = "fail";
+
 
 $u = "";
 $p = "";
+$wat = "";
 
 #read passwords
 $handle = fopen("records.csv", "r");
 if ($handle) {
 	while (($line = fgets($handle)) !== false && $l==0) {
-		$a = explode(',', $line);
+		$a = array_map('trim', explode(',', $line));
 		if ($a[1] == $uname && $a[5] == "success") {
 			$u = $a[1];
-			if ($a[2] == $passId) {
-				$l = 1;				
+			$wat = $a[6];
+			if ($a[2] == $passId && $a[6] == $pwd) {
+				$l = 1;
+				$p = $a[2];		
 			}
 		}
 	}
@@ -33,14 +40,13 @@ fclose($handle);
 if ($l == 1) $r = "success";
 if($u) {
 	$file = fopen("records.csv","a");
-	$s =date('Y-m-d H:i:s').",".$uname.",".$passId.",".$attemptDuration.",".$attemptCount.",".$r."\n";
+	$s =date('Y-m-d H:i:s').",".$uname.",".$passId.",".$attemptDuration.",".$attemptCount.",".$r.",".$pwd."\n";
 	fwrite($file,$s);
 	fclose($file);
 } else {
 	$u = "null";
 }
 
-#HOW TO PASS VALIDATION OUTCOME TO BRAD? $l or $r?
-echo $u.",".$r
-print 'done';
+// echo $u.",".$r.",".$p.",".$passId.",".$wat.",".$pwd;
+echo $u.",".$r.";
 ?>
